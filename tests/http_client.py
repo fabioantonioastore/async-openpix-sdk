0a9f23534605784e7969c72138ad10b6e2119d56
ninja <astore.a.fabio@gmail.com>
2025-05-23 22:21:54 -0300

    refactor

--- tests/http_client.py
+++ tests/http_client.py
@@ -1,10 +1,10 @@
 from asyncio import run
-import json
 
 from openpix.http import AsyncHTTPClient
 
 url = "https://run.mocky.io/v3/a4c6c63c-524c-4a54-9e92-b77d904bbd4e"
 url2 = "https://run.mocky.io/v3/e1019553-7c38-46f9-abcb-8cd4767fdbf4"
+
 
 async def main():
     async with AsyncHTTPClient() as http_client:
@@ -16,5 +16,6 @@ async def main():
 
         async for item in http_client.get_stream(url2):
             print(item)
+
 
 run(main())
